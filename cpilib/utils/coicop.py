a8ca0848d05748a3c2b5cb85af481ff2a3021df9
Thomas Kientz <thomas@kientz.net>
2023-12-19 15:53:41 +0100

    Update dtype for code column in coicop_labels

--- cpilib/utils/coicop.py
+++ cpilib/utils/coicop.py
@@ -2,4 +2,4 @@ import pandas as pd
 import pkg_resources
 
 abs_file_path = pkg_resources.resource_filename("cpilib", "data/hicp_labels.csv")
-coicop_labels = pd.read_csv(abs_file_path)
+coicop_labels = pd.read_csv(abs_file_path, dtype={"code": str})
